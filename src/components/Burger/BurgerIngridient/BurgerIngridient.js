c9665f2bf012ef47df7026596d4a1bc2a223c557
DESKTOP-U0HC7KA\ASUS <yarzartun951@gmail.com>
2021-08-17 20:40:05 +0630

    [add] [burger]

--- src/components/Burger/BurgerIngridient/BurgerIngridient.js
+++ src/components/Burger/BurgerIngridient/BurgerIngridient.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-import classes from './BurgerIngredient.css';
+import classes from './BurgerIngridient.css';
 
 class BurgerIngredient extends Component {
     render () {
         let ingredient = null;
 
-        switch (props.type) {
+        switch ( this.props.type ) {
             case ('bread-bottom'):
                 ingredient = <div className={classes.BreadBottom}></div>;
                 break;
@@ -33,13 +33,14 @@ class BurgerIngredient extends Component {
                 break;
             default:
                 ingredient = null;
-
-            return ingredient;
+            
         }
+
+        return ingredient;
     }
 } 
 
-BurgerIngredient.PropTypes = {
+BurgerIngredient.propTypes = {
     type: PropTypes.string.isRequired
 };
 
