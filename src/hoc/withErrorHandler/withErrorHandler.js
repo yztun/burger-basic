531e39ed3fb669f44cf208f0ae128178ee3a9be3
DESKTOP-U0HC7KA\ASUS <yarzartun951@gmail.com>
2021-08-26 11:19:20 +0630

    [remove] [old interceptor]

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -7,17 +7,22 @@ const withErrorHandler = ( WrappedComponent, axios ) => {
     return class extends Component {
 
         state = {
-            error: null
+            error: null,
         }
 
         componentWillMount () {
-            axios.interceptors.request.use(req => {
+            this.reqInterceptor = axios.interceptors.request.use(req => {
                 this.setState({error: null});
                 return req;
             });
-            axios.interceptors.response.use(res => res, error => {
+            this.reqInterceptor = axios.interceptors.response.use(res => res, error => {
                 this.setState({error: error});
             });
+        }
+
+        componentWillUnmount () {
+            axios.interceptors.request.eject(this.reqInterceptor);
+            axios.interceptors.response.eject(this.reqInterceptor);
         }
 
         errorConfirmedHandler = () => {
